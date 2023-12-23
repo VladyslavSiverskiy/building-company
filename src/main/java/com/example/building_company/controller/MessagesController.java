b5c48ce4afd5869bdc1bc9d9adc9a70c74abf5d6
VladyslavSiverskiy <v.siverskiy@gmail.com>
2023-12-23 20:14:42 +0200

    Add number validation

--- src/main/java/com/example/building_company/controller/MessagesController.java
+++ src/main/java/com/example/building_company/controller/MessagesController.java
@@ -18,13 +18,15 @@ public class MessagesController {
 
     @PostMapping
     public String sentToMe(@RequestParam(name = "phoneNumber") String phoneNumber, Model model) {
-        if (phoneNumber.isEmpty()) {
-            model.addAttribute("errorMessage", "Podaj numer telefonu!");
-            return homeController.openHomePage(model);
+        String regex = "^[\\+]?[(]?[0-9]{3}[)]?[-\\s\\.]?[0-9]{3}[-\\s\\.]?[0-9]{4,6}$";
+        String phoneNumberWithoutSymbols;
+        phoneNumberWithoutSymbols = phoneNumber.replaceAll("\\D", "");
+        if (phoneNumber.isEmpty() || !phoneNumberWithoutSymbols.matches(regex)) {
+            model.addAttribute("errorMessage", "Nieprawidłowy format numeru telefonu!");
         }else {
             emailService.sendEmail("v.siverskiy@gmail.com", "Hello, Nazar!", phoneNumber);
             model.addAttribute("successMessage", "Wiadomość została wysłana pomyślnie!");
-            return homeController.openHomePage(model);
         }
+        return homeController.openHomePage(model);
     }
 }
